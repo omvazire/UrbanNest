faadeeacfeaa91d5017dd3b03204e9a956a51578
om vazire <omvazire1195@gmail.com>
2025-12-27 12:38:32 +0530

    learned hashing, salting, used tools passport, passport-local, passport-local-mongoose, created a schema for user in models and make a demo user

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate = require("ejs-mate");
 const expressError = require("./utils/expressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -48,6 +51,13 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()))
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 main()
     .then(() => {
@@ -62,6 +72,18 @@ app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+
+
+
+app.get("/demouser", async (req,res)=>{
+let fakeuser = new User({
+    email: "student@gmail.com",
+    username: "new-student",
+});
+
+let registeruser = await User.register(fakeuser, "helloworld");
+res.send(registeruser);
 });
 
 
